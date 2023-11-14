f1d1b0715e1130c612384fd63c982ed07219e5fa
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-11-14 14:20:35 -0300

    Add files via upload

---
+++ src/main/java/br/edu/ifpi/entidades/Administrador.java
@@ -0,0 +1,9 @@
+package br.edu.ifpi.entidades;
+
+public class Administrador extends Usuario{
+
+    public Administrador(String nome, int id, String email) {
+        super(nome, id, email);
+    }
+
+}
