3159c895a1d7caaf9038e5e90f7c4b48176c12ea
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-11-17 14:34:20 -0300

    add novos menus

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -2,21 +2,24 @@ package br.edu.ifpi;
 
 import java.util.Scanner;
 
+import br.edu.ifpi.DAO.AlunoDao;
+import br.edu.ifpi.DAO.ProfessorDao;
 import br.edu.ifpi.entidades.Administrador;
 import br.edu.ifpi.entidades.Aluno;
 import br.edu.ifpi.entidades.Professor;
 import br.edu.ifpi.entidades.Usuario;
-
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Bem Vindo ao Sistema de Cursos Online JTM");
         mostrarMenuInicial();
     }
+    
     public static void mostrarMenuInicial(){
-        System.out.println("M E N U   I N I C I A L");
+        System.out.println("___________M E N U   I N I C I A L___________");
         System.out.println("1. Cadastrar Usuário");
         System.out.println("2. Logar Usuário");
+        System.out.println("_____________________________________________");
 
         Scanner scanner = new Scanner(System.in);
 
@@ -27,51 +30,96 @@ public class App {
         if (opt == 1){
             cadastraUsuario();
         } else if (opt == 2){
-            logarUsuario();
+
         }else{
             System.out.println("Opção Inválida, tente Novamente");
             mostrarMenuInicial();
         }
     }
-}
 
     public static void cadastraUsuario(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha o tipo de Usuário:");
+        System.out.println("1. Aluno");
+        System.out.println("2. Professor");
+        System.out.println("3. Administrador");
+        int tipo = scanner.nextInt();
+        scanner.nextLine();
+
+        if (tipo == 1){
+            System.out.println("Digite seu Nome:");
+            String nome = scanner.nextLine();
+            System.out.println("Digite seu Email:");
+            String email = scanner.nextLine();
+
+            Aluno aluno = new Aluno(nome, tipo, email, null);
+            AlunoDao.cadastrar(aluno);
+        } else if (tipo == 2){
+            System.out.println("Digite seu Nome:");
+            String nome = scanner.nextLine();
+            System.out.println("Digite seu Email:");
+            String email = scanner.nextLine();
+
+            Professor professor = new Professor(nome, tipo, email, null);
+            ProfessorDao.cadastrar(professor);
+        } else if (tipo == 3){
+            System.out.println("Digite seu Nome:");
+            String nome = scanner.nextLine();
+            System.out.println("Digite seu Email:");
+            String email = scanner.nextLine();
+            
+            Administrador administrador = new Administrador(nome, tipo, email);
+            AdministradorDao.cadastrar(administrador);
+        }
+    }
+    
+    public static void logarUsuario(){
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite seu Nome:");
         String nome = scanner.nextLine();
-        System.out.println("Digite seu email:");
-        String email = scanner.nextLine();
-        System.out.println("Digite sua senha:");
-        String senha = scanner.nextLine();
+        System.out.println("Digite seu ID:");
+        int idUsuario = scanner.nextInt();
+        scanner.nextLine();
 
-        Usuario usuario = new Usuario(nome, email, senha);
-
-        UsuarioDAO dao = new UsuarioDAO();
-        if (dao.cadastrar(usuario) > 0) {
-            System.out.println("Usuário cadastrado com sucesso!");
-        } else {
-            System.out.println("Erro ao cadastrar o usuário!");
-        }
-        mostrarMenuInicial();
     }
 
-    public static void logarUsuario(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Digite seu email:");
-        String email = scanner.nextLine();
-        System.out.println("Digite sua senha:");
-        String senha = scanner.nextLine();
-
-        UsuarioDAO dao = new UsuarioDAO();
-        Usuario usuario = dao.buscarPorEmail(email);
-
-        if (usuario != null && usuario.getSenha().equals(senha)){
-            System.out.println("Usuário logado com sucesso!");
-      
-        } else {
-            System.out.println("Email ou senha incorretos!");
-            mostrarMenuInicial();
-        }
+    public static void menuAluno(){
+        System.out.println("________M E N U   D O   A L U N O________");
+        System.out.println("\n1. Vizualizar lista de Cursos");
+        System.out.println("2. Vizualizar lista de Professores");
+        System.out.println("3. Vizualizar Notas");
+        System.out.println("4. Realizar cadastro em Curso");
+        System.out.println("5. Sair");
+        System.out.println("_____________________________________________");
     }
+
+    public static void menuProfessor(){
+        System.out.println("______M E N U   D O   P R O F E S S O R______");
+        System.out.println("\n1. Vizualizar lista de Cursos");
+        System.out.println("2. Vizualizar lista de Professores");
+        System.out.println("3. Vizualizar lista de Alunos");
+        System.out.println("4. Realizar cadastro em Curso");
+        System.out.println("5. Registrar Notas de Alunos");
+        System.out.println("6. Sair");
+        System.out.println("_____________________________________________");
+    }
+
+    public static void menuAdministrador(){
+        System.out.println("___M E N U   D O   A D M I N S T R A D O R___");
+        System.out.println("\n1. Vizualizar lista de Cursos");
+        System.out.println("2. Cadastrar Curso");
+        System.out.println("3. Editar Curso");
+        System.out.println("4. Excluir Curso");
+        System.out.println("5. Vizualizar lista de Professores");
+        System.out.println("6. Cadastrar Professor");
+        System.out.println("7. Editar Professor");
+        System.out.println("8. Excluir Professor");
+        System.out.println("9. Vizualizar lista de Alunos");
+        System.out.println("10. Cadastrar Aluno");
+        System.out.println("11. Editar Aluno");
+        System.out.println("12. Excluir Aluno");
+        System.out.println("13. Sair");
+        System.out.println("_____________________________________________");
+    }
+}
