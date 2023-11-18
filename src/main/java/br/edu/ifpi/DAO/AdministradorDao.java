c04e90436fb402cd96748fa4dcf78345752204bf
Joao <joaofernandesrodrigues78@gmail.com>
2023-11-18 15:21:50 -0300

    SQL

--- src/main/java/br/edu/ifpi/DAO/AdministradorDao.java
+++ src/main/java/br/edu/ifpi/DAO/AdministradorDao.java
@@ -7,36 +7,28 @@ import java.util.List;
 
 import br.edu.ifpi.entidades.Administrador;
 
-class AdministradorDao implements Dao<Administrador>{
+class AdministradorDao implements Dao<Administrador> {
     private Connection conexao;
-    @Override
-    public int cadastrar(Administrador adiministrador) {
-          String SQL_INSERT = "INSERT INTO Administrador (NOME, EMAIL) VALUES(?,?)";
 
-        try{
-            PreparedStatement stmt =  conexao.prepareStatement(SQL_INSERT);
-            stmt.setString(1, adiministrador.getNome());
-            stmt.setString(2, adiministrador.getEmail());
-        }catch (SQLException e){
-            System.err.format("SQL State %s\n%s", e.getSQLState(), e.getMessage());
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return 0;  
-      }
-
-    @Override
-    public List<Administrador> consultarTodos() {
-        throw new UnsupportedOperationException("Unimplemented method 'consultarTodos'");
+    public AdministradorDao(Connection conexao) {
+        this.conexao = conexao;
     }
 
     @Override
-    public int alterar(Administrador adiministrador) {
+    public int cadastrar(Administrador administrador) {
+        String SQL_INSERT = "INSERT INTO Administrador (NOME, EMAIL) VALUES(?,?)";
+
         try {
-            String sqlUpdate = "UPDATE administrador SET NOME=?, EMAIL=? WHERE ID=?" + adiministrador;
-            PreparedStatement stmt = conexao.prepareStatement(sqlUpdate);
-            stmt.setString(1, adiministrador.getNome());
-            stmt.setString(2, adiministrador.getEmail());
+            PreparedStatement stmt = conexao.prepareStatement(SQL_INSERT);
+            stmt.setString(1, administrador.getNome());
+            stmt.setString(2, administrador.getEmail());
+
+            stmt.executeUpdate();
+
+            stmt.close();
+
+        } catch (SQLException e) {
+            System.err.format("SQL State %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -44,7 +36,31 @@ class AdministradorDao implements Dao<Administrador>{
     }
 
     @Override
-    public int remover(Administrador entidade) {
-        throw new UnsupportedOperationException("Unimplemented method 'remover'");
+    public List<Administrador> consultarTodos() {
+        throw new UnsupportedOperationException("Método 'consultarTodos' não implementado");
     }
-    
+
+    @Override
+    public int alterar(Administrador administrador) {
+        try {
+
+            String sqlUpdate = "UPDATE Administrador SET NOME=?, EMAIL=? WHERE ID=?";
+            PreparedStatement stmt = conexao.prepareStatement(sqlUpdate);
+            stmt.setString(1, administrador.getNome());
+            stmt.setString(2, administrador.getEmail());
+
+            stmt.executeUpdate();
+
+            stmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public int remover(Administrador entidade) {
+        throw new UnsupportedOperationException("Método 'remover' não implementado");
+    }
+}
