00cf443686e3c1927271e58db0879efa321a27cb
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-11-18 14:39:21 -0300

    Update App.java

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -82,30 +82,85 @@ public class App {
         int idUsuario = scanner.nextInt();
         scanner.nextLine();
 
+        /* bucar pelo id e identificar o tipo, se for aluno = 1, prof = 2, adm = 3 */
+
+        if (tipo == 1){
+            menuAluno();
+        } else if (tipo == 2){
+            menuProfessor();
+        } else if (tipo == 3){
+            menuAdministrador();
+        } else {
+            System.out.println("Usuário Não Encontrado, tente novamente.");
+            logarUsuario();
+        }
+
     }
 
     public static void menuAluno(){
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("________M E N U   D O   A L U N O________");
         System.out.println("\n1. Vizualizar lista de Cursos");
         System.out.println("2. Vizualizar lista de Professores");
         System.out.println("3. Vizualizar Notas");
         System.out.println("4. Realizar cadastro em Curso");
-        System.out.println("5. Sair");
+        System.out.println("0. Sair");
         System.out.println("_____________________________________________");
+        System.out.println("\nEscolha uma opção: ");
+        Int opt = scanner.nextInt();
+        scanner.nextLine();
+
+        if(opt == 1){
+
+        } else if (opt == 2){
+
+        } else if (opt == 3){
+
+        } else if (opt == 4){
+
+        }else if (opt == 0){
+            mostrarMenuInicial();
+        } else {
+            System.out.println("Opção Inválida, tente novamente.");
+            menuAluno();
+        }
     }
 
     public static void menuProfessor(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("______M E N U   D O   P R O F E S S O R______");
         System.out.println("\n1. Vizualizar lista de Cursos");
         System.out.println("2. Vizualizar lista de Professores");
         System.out.println("3. Vizualizar lista de Alunos");
         System.out.println("4. Realizar cadastro em Curso");
         System.out.println("5. Registrar Notas de Alunos");
-        System.out.println("6. Sair");
+        System.out.println("0. Sair");
         System.out.println("_____________________________________________");
+        System.out.println("\nEscolha uma opção: ");
+        Int opt = scanner.nextInt();
+        scanner.nextLine();
+
+        if(opt == 1){
+
+        } else if (opt == 2){
+
+        } else if (opt == 3){
+
+        } else if (opt == 4){
+
+        }else if (opt == 5){
+            
+        }else if (opt == 0){
+            mostrarMenuInicial();
+        } else {
+            System.out.println("Opção Inválida, tente novamente.");
+            menuProfessor();
+        }
     }
 
     public static void menuAdministrador(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("___M E N U   D O   A D M I N S T R A D O R___");
         System.out.println("\n1. Vizualizar lista de Cursos");
         System.out.println("2. Cadastrar Curso");
@@ -119,7 +174,39 @@ public class App {
         System.out.println("10. Cadastrar Aluno");
         System.out.println("11. Editar Aluno");
         System.out.println("12. Excluir Aluno");
-        System.out.println("13. Sair");
+        System.out.println("0. Sair");
         System.out.println("_____________________________________________");
+        System.out.println("\nEscolha uma opção: ");
+        Int opt = scanner.nextInt();
+        scanner.nextLine();
+
+        if(opt == 1){
+
+        } else if (opt == 2){
+
+        } else if (opt == 3){
+
+        } else if (opt == 4){
+
+        }else if (opt == 5){
+            
+        }else if (opt == 6){
+            
+        }else if (opt == 7){
+            
+        }else if (opt == 8){
+            
+        }else if (opt == 10){
+            
+        }else if (opt == 11){
+            
+        }else if (opt == 12){
+            
+        }else if (opt == 0){
+            mostrarMenuInicial();
+        } else {
+            System.out.println("Opção Inválida, tente novamente.");
+            menuAdministrador();
+        }
     }
 }
