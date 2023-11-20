f60fe2e7eee829f455dcdeb6f122198c6f829bae
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-11-20 14:12:44 -0300

    .

--- src/main/java/br/edu/ifpi/entidades/Administrador.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpi.entidades;
-
-public class Administrador extends Usuario{
-
-    public Administrador(String nome, int id, String email) {
-        super(nome, id, email);
-    }
-
-}
