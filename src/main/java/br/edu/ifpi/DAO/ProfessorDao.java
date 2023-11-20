2f979d092f8b94cee45e2f6741844520addad988
João Fernandes <137800495+billiezinha@users.noreply.github.com>
2023-11-20 13:58:11 -0300

    Update ProfessorDao.java

--- src/main/java/br/edu/ifpi/DAO/ProfessorDao.java
+++ src/main/java/br/edu/ifpi/DAO/ProfessorDao.java
@@ -2,7 +2,9 @@ package br.edu.ifpi.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.edu.ifpi.entidades.Professor;
@@ -37,17 +39,79 @@ public class ProfessorDao implements Dao<Professor> {
 
         return 0;
     }
+    
     @Override
-    public List<Professor>consultarTodos(){
-        return null;
-    }
-    @Override
-    public int alterar(Professor entidade){
-        throw new UnsupportedOperationException("Unimplemented method 'alterar'");
-    }
-    @Override
-    public int remover(Professor entidade){
-        throw new UnsupportedOperationException("Unimplemented method 'remove' ");
+    public List<Professor> consultarTodos() {
+        List<Professor> professores = new ArrayList<>();
+        String SQL_SELECT_ALL = "SELECT * FROM PROFESSOR";
+
+        try {
+            PreparedStatement preparedStatement = conexao.prepareStatement(SQL_SELECT_ALL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Professor professor = new Professor();
+                professor.setId(resultSet.getInt("ID")); // Assuming there is an ID column
+                professor.setNome(resultSet.getString("NOME"));
+                professor.setEmail(resultSet.getString("EMAIL"));
+
+                professores.add(professor);
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return professores;
     }
 
+    @Override
+    public int alterar(Professor professor) {
+        String SQL_UPDATE = "UPDATE PROFESSOR SET NOME=?, EMAIL=? WHERE ID=?";
+
+        try {
+            PreparedStatement preparedStatement = conexao.prepareStatement(SQL_UPDATE);
+
+            preparedStatement.setString(1, professor.getNome());
+            preparedStatement.setString(2, professor.getEmail());
+            preparedStatement.setInt(3, professor.getId()); // Assuming there is an ID column
+
+            int row = preparedStatement.executeUpdate();
+
+            System.out.println(row);
+            return row;
+
+        } catch (SQLException e) {
+            System.err.format("SQL State %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    @Override
+    public int remover(Professor professor) {
+        String SQL_DELETE = "DELETE FROM PROFESSOR WHERE ID=?";
+
+        try {
+            PreparedStatement preparedStatement = conexao.prepareStatement(SQL_DELETE);
+
+            preparedStatement.setInt(1, professor.getId()); // Assuming there is an ID column
+
+            int row = preparedStatement.executeUpdate();
+
+            System.out.println(row);
+            return row;
+
+        } catch (SQLException e) {
+            System.err.format("SQL State %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
 }
