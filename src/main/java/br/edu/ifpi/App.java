d421e96390e401b89ce6795a06026741300bb1e0
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-11-20 21:31:45 -0300

    remove adm

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -2,11 +2,10 @@ package br.edu.ifpi;
 
 import java.util.Scanner;
 
-import br.edu.ifpi.DAO.AdministradorDao;
 import br.edu.ifpi.DAO.AlunoDao;
 import br.edu.ifpi.DAO.Conexao;
+import br.edu.ifpi.DAO.CursoDao;
 import br.edu.ifpi.DAO.ProfessorDao;
-import br.edu.ifpi.entidades.Administrador;
 import br.edu.ifpi.entidades.Aluno;
 import br.edu.ifpi.entidades.Professor;
 
@@ -44,7 +43,6 @@ public class App {
         System.out.println("Escolha o tipo de Usuário:");
         System.out.println("1. Aluno");
         System.out.println("2. Professor");
-        System.out.println("3. Administrador");
         int tipo = scanner.nextInt();
         scanner.nextLine();
 
@@ -70,18 +68,7 @@ public class App {
 
             Professor professor = new Professor(nome, tipo, email, null);
             ProfessorDao.cadastrar(professor);
-        } else if (tipo == 3){
-            System.out.println("Digite seu Nome:");
-            String nome = scanner.nextLine();
-            System.out.println("Digite seu Email:");
-            String email = scanner.nextLine();
-
-            Conexao conexao = new Conexao();
-            AdministradorDao AdministradorDao = new AdministradorDao(conexao);
-            
-            Administrador administrador = new Administrador(nome, tipo, email);
-            AdministradorDao.cadastrar(administrador);
-        }
+        } 
     }
     
     public static void logarUsuario(){
@@ -99,8 +86,6 @@ public class App {
             menuAluno();
         } else if (tipo == 2){
             menuProfessor();
-        } else if (tipo == 3){
-            menuAdministrador();
         } else {
             System.out.println("Usuário Não Encontrado, tente novamente.");
             logarUsuario();
@@ -110,6 +95,7 @@ public class App {
 
     public static void menuAluno(){
         Scanner scanner = new Scanner(System.in);
+        Conexao conexao = new Conexao();
 
         System.out.println("________M E N U   D O   A L U N O________");
         System.out.println("\n1. Vizualizar lista de Cursos");
@@ -123,9 +109,9 @@ public class App {
         scanner.nextLine();
 
         if(opt == 1){
-
+            CursoDao cursodao = new CursoDao(conexao);
+            CursoDao.consultarTodos();
         } else if (opt == 2){
-            Conexao conexao = new Conexao();
             ProfessorDao professordao = new ProfessorDao(conexao);
             ProfessorDao.consultarTodos();
         } else if (opt == 3){
@@ -142,6 +128,8 @@ public class App {
 
     public static void menuProfessor(){
         Scanner scanner = new Scanner(System.in);
+        Conexao conexao = new Conexao();
+
         System.out.println("______M E N U   D O   P R O F E S S O R______");
         System.out.println("\n1. Vizualizar lista de Cursos");
         System.out.println("2. Vizualizar lista de Professores");
@@ -155,9 +143,9 @@ public class App {
         scanner.nextLine();
 
         if(opt == 1){
-
+            CursoDao cursodao = new CursoDao(conexao);
+            CursoDao.consultarTodos();
         } else if (opt == 2){
-            Conexao conexao = new Conexao();
             ProfessorDao professordao = new ProfessorDao(conexao);
             ProfessorDao.consultarTodos();
             menuProfessor();
@@ -172,60 +160,6 @@ public class App {
         } else {
             System.out.println("Opção Inválida, tente novamente.");
             menuProfessor();
-        }
-    }
-
-    public static void menuAdministrador(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("___M E N U   D O   A D M I N S T R A D O R___");
-        System.out.println("\n1. Vizualizar lista de Cursos");
-        System.out.println("2. Cadastrar Curso");
-        System.out.println("3. Editar Curso");
-        System.out.println("4. Excluir Curso");
-        System.out.println("5. Vizualizar lista de Professores");
-        System.out.println("6. Cadastrar Professor");
-        System.out.println("7. Editar Professor");
-        System.out.println("8. Excluir Professor");
-        System.out.println("9. Vizualizar lista de Alunos");
-        System.out.println("10. Cadastrar Aluno");
-        System.out.println("11. Editar Aluno");
-        System.out.println("12. Excluir Aluno");
-        System.out.println("0. Sair");
-        System.out.println("_____________________________________________");
-        System.out.println("\nEscolha uma opção: ");
-        int opt = scanner.nextInt();
-        scanner.nextLine();
-
-        if(opt == 1){
-
-        } else if (opt == 2){
-
-        } else if (opt == 3){
-
-        } else if (opt == 4){
-
-        }else if (opt == 5){
-            Conexao conexao = new Conexao();
-            ProfessorDao professordao = new ProfessorDao(conexao);
-            ProfessorDao.consultarTodos();
-            menuAdministrador();
-        }else if (opt == 6){
-            
-        }else if (opt == 7){
-            
-        }else if (opt == 8){
-            
-        }else if (opt == 10){
-            
-        }else if (opt == 11){
-            
-        }else if (opt == 12){
-            
-        }else if (opt == 0){
-            mostrarMenuInicial();
-        } else {
-            System.out.println("Opção Inválida, tente novamente.");
-            menuAdministrador();
         }
     }
 }
