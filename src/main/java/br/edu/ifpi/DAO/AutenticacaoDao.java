b1d84eb3a2d08b3aefe10699a2dff911a9c60ec2
thaylanni <137801828+thaylanni@users.noreply.github.com>
2023-11-23 17:51:02 -0300

    Update AutenticacaoDao.java

--- src/main/java/br/edu/ifpi/DAO/AutenticacaoDao.java
+++ src/main/java/br/edu/ifpi/DAO/AutenticacaoDao.java
@@ -1,9 +1,62 @@
-.package br.edu.ifpi.DAO;
+package br.edu.ifpi.DAO;
 
-/**
- * AutenticacaoDao
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.edu.ifpi.entidades.Aluno;
+import br.edu.ifpi.entidades.Professor;
+
 public class AutenticacaoDao {
+    private Connection conexao;
 
-    
+    public AutenticacaoDao(Connection conexao) {
+        this.conexao = conexao;
+    }
+
+    public Aluno autenticarAluno(String email) throws SQLException {
+        String sql = "SELECT id, nome, email FROM aluno WHERE email = ?";
+
+        try (PreparedStatement stm = conexao.prepareStatement(sql)) {
+            stm.setString(1, email);
+            try (ResultSet resultSet = stm.executeQuery()) {
+                if (resultSet.next()) {
+                    String nome = resultSet.getString("nome");
+
+                    System.out.println("Aluno autenticado com sucesso!");
+                    return new Aluno(nome, email);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Erro ao autenticar aluno: " + e.getMessage());
+        }
+
+        System.out.println("Aluno não encontrado!");
+        return null;
+    }
+
+    public Professor autenticarProfessor(String email) throws SQLException {
+        String sql = "SELECT id, nome, email FROM professor WHERE email = ?";
+
+        try (PreparedStatement stm = conexao.prepareStatement(sql)) {
+            stm.setString(1, email);
+            try (ResultSet resultSet = stm.executeQuery()) {
+                if (resultSet.next()) {
+                    String nome = resultSet.getString("nome");
+
+                    System.out.println("Professor autenticado com sucesso!");
+                    return new Professor(nome, email);
+                }
+            }
+        } catch (SQLException e) {
+            
+            e.printStackTrace();
+            throw new SQLException("Erro ao autenticar professor: " + e.getMessage());
+        }
+
+        System.out.println("Professor não encontrado!");
+        return null;
+    }
 }
